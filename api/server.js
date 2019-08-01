f368a0924d14ab275479e0fa5ba21bed74beb36a
Dil Sombayeva <dilyasom@gmail.com>
2019-07-31 19:50:33 -0700

    add motd

--- api/server.js
+++ api/server.js
@@ -9,25 +9,27 @@ server.use(helmet());
 server.use(express.json());
 
 server.get('/', async (req, res) => {
-  try {
-    const shouts = await db('shouts');
-    res.status(200).json(shouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shouts' });
-  }
+ try {
+  const shouts = await db('shouts');
+  const messageOfTheDay = process.env.MOTD || 'Hi World!';
+
+  res.status(200).json({ motd: messageOfTheDay, shouts });
+ } catch (error) {
+  console.error('\nERROR', error);
+  res.status(500).json({ error: 'Cannot retrieve the shouts' });
+ }
 });
 
 server.post('/', async (req, res) => {
-  try {
-    const [id] = await db('shouts').insert(req.body);
-    const shouts = await db('shouts');
+ try {
+  const [id] = await db('shouts').insert(req.body);
+  const shouts = await db('shouts');
 
-    res.status(201).json(shouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shout' });
-  }
+  res.status(201).json(shouts);
+ } catch (error) {
+  console.error('\nERROR', error);
+  res.status(500).json({ error: 'Cannot add the shout' });
+ }
 });
 
 module.exports = server;
